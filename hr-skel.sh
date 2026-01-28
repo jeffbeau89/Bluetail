a47ed6c08a61a085b619fd09ca7f4e2dcc9a07da
Jeff Beau <jeffbeauplan89@gmail.com>
2026-01-28 01:33:24 -0500

    HR directory sync

--- hr-skel.sh
+++ hr-skel.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 
 #EVERYONE IN THIS GROUP HAS AN UNIQUE SKELETON FOLDER
-#COPY ALL FILES IN THE FOLDER THAT HAVE BEEN RECENTLY MODIFIED AND SEND THEM TO ALL THE USERS IN THAT GROUP
+#MAKE SURE THE HOME DIRECTORIES OF ALL GROUP MEMBERS ARE ALWAYS SYNCED WITH THE FILES IN THE SKELETON DIRECTORY:
 
-echo "HR Documents last updated $(date)" >> /home/user/change.log
 
 GROUP="HR"
 
 USERS=$(getent group "$GROUP" | cut -d: -f4 | cut -d, -f1- | tr ',' '\n')
-echo $USERS
+#echo $USERS
+
+
+logger -t onboarding "Change detected in "$GROUP" directory."
+logger -t onboarding "Syncing Initiated for all "$GROUP" department members."
+logger -t onboarding "Home Directory is being updated for the following users:"
 
 for user in $USERS;
 do
-        echo "$user's Home Directory is now being updated"
-    find /bluetail/HR -type f -mmin -10 -exec cp {} /home/$user \;
-        echo "$user's Home Directory has been updated"
+
+    rsync -a --delete /bluetail/$GROUP/ /home/$user/
+    logger -t onboarding "$user"
+
+
+    #ORIGINALLY: find /bluetail/HR -type f -mmin -1 -exec cp {} /home/$user \;
+    #rsync is more effecient than find command in this scenario, as it compare directories and can both copy delete necessary files
+    #In return syncs the target directory with the skeleton directory.
+    #The "-mmin -1" option can cause duplicates if skeleton directory is updated multiple times within a minute.
 
 done
+
+logger -t onboarding "Syncing Complete. $(date)"
+
+~
 ~
