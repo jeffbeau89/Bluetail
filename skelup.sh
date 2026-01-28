917ad23b8f08698559ce1f6294a5d84a3097c4ff
Jeff Beau <jeffbeauplan89@gmail.com>
2026-01-28 01:34:44 -0500

    Skeleton directory sync

--- skelup.sh
+++ skelup.sh
@@ -2,9 +2,19 @@
 
 #COPY ALL MODIFIED FILES IN SKEL DIRECTORY TO ALL HOME DIRECTORIES
 
-echo "/etc/skel was updated $(date)" >> /home/user/change.log
 
-for dest in /home/*;
+for user in /home/*;
 do
-	find /etc/skel -type f -mmin -10 -exec cp {} $dest \;
+
+
+logger -t onboarding "Change detected in main skeleton directory on $(date)"
+logger -t onboarding "Syncing files for all general staff"
+
+
+    rsync -a --delete /etc/skel /home/$user/
+    logger -t onboarding "Syncing Complete: Home directories has been updated for all general staff."
+
+
+
+
 done 
